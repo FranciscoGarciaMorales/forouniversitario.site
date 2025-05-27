9597fa89d98ed7eb3a59e59bf3788a4c35fa8981
Yuriko <yurikosean@gmail.com>
2025-05-27 21:23:39 +0200

    actualización de rutas
    Correccion en rutas, se pusieron rutas absolutas.

--- inc/footer.php
+++ inc/footer.php
@@ -2,9 +2,9 @@
 <div class="b-example-divider"></div>
 <footer class="py-3 my-4 text-center mt-auto">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-      <li class="nav-item"><a href="index.php" class="nav-link px-2 text-body-secondary">Inicio</a></li>
-      <li class="nav-item"><a href="inc/terminos.html" class="nav-link px-2 text-body-secondary">Term. & Cond.</a></li>
-      <li class="nav-item"><a href="inc/contacto.php" class="nav-link px-2 text-body-secondary">Contacto</a></li>
+      <li class="nav-item"><a href="/forouniversitario.siteGithub/index.php" class="nav-link px-2 text-body-secondary">Inicio</a></li>
+      <li class="nav-item"><a href="/forouniversitario.siteGithub/terminos.html" class="nav-link px-2 text-body-secondary">Term. & Cond.</a></li>
+      <li class="nav-item"><a href="/forouniversitario.siteGithub/contacto.php" class="nav-link px-2 text-body-secondary">Contacto</a></li>
     </ul>
     <p class="text-center text-body-secondary text-footer">&copy; 2025 Foro Universitario, Group 19</p>
     <!-- Validación de CSS -->
